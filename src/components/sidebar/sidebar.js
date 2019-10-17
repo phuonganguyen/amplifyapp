95abdce6e3c5a947dc8ed668d300fd48b97cdb47
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2019-10-17 17:51:41 +0700

    Update sidebar.js

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -36,13 +36,13 @@ export default class Sidebar extends Component{
                 <div className="contact">
                     <div><a href="#">Get in touch</a></div>
                     <div className="social-icons">
-                        <a rel="me" href="#" className="button button-icon">
+                        <a rel="me" href="mailto:phuong.nguyenanh06@gmail.com" className="button button-icon" title="mail: phuong.nguyenanh06@gmail.com">
                             <i className="fa fa-envelope"></i>
                         </a>
-                        <a rel="me" href="#" className="button button-icon">
+                        <a rel="me" href="https://www.linkedin.com/in/phuong-nguyen-5b214862/" className="button button-icon">
                             <i className="fa fa-linkedin-square"></i>
                         </a>
-                        <a rel="me" href="#" className="button button-icon">
+                        <a rel="me" href="https://github.com/phuonganguyen" className="button button-icon">
                             <i className="fa fa-github-square"></i>
                         </a>
                         <a rel="me" href="#" className="button button-icon">
